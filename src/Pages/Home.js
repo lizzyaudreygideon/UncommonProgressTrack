019cf7c2a56fda9b08035af61a35e08457597ff4
audreylizzy <lizzyaudreygideon@gmail.com>
2024-10-31 00:33:11 +0200

    BannerImage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import scratchBlockCodingImage from '../Components/Gallery/scratch_beginners.jpg';
 
 const Home = () => {
   return (
-    <div>
+    <div 
+    style={{ 
+      backgroundImage: `url(${scratchBlockCodingImage})`, 
+      backgroundSize: 'cover', 
+      backgroundPosition: 'center', 
+      height: '87vh', 
+      width: '100%' 
+    }} 
+    className="bg-no-repeat"
+  >
 
-        
       
     </div>
   )
