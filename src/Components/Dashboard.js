6f93937f2988e98e1cd265d32489322ffc695093
audreylizzy <lizzyaudreygideon@gmail.com>
2024-10-30 22:31:16 +0200

    Navigation Bar

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const MyComponent = () => {
   return (
-    <div class="bg-blue-500 text-green p-4">
-      This is a Tailwind CSS styled component.
+    <div >
+      
     </div>
   );
 };
